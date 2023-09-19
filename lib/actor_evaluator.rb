66a9a79c3e0df4244253e3ed0c3d81ccbd403646
yokotateyoko <145460674+yokotateyoko@users.noreply.github.com>
2023-09-19 22:57:15 +0900

    9/19 作業分。明日は 1st 2nd から

--- lib/actor_evaluator.rb
+++ lib/actor_evaluator.rb
@@ -1,12 +1,24 @@
 # +(+(1,1), 1) -> +(2, 1)
 def reduce(ast)
     case ast
-    in {type: 'bin_exp_app', ope:'+', left: left, right: right}
-        return mk_bin_exp('+', reduce(left), right) unless left.val?
-        return mk_bin_exp('+', left, reduce(right)) unless right.val?
-        case [left, right]
-        in [{type:'nat', value:left_val}, {type:'nat', value:right_val}]
-            return mk_nat(right_val + left_val)
+    in {type: 'bin_exp_app', ope:ope, left: left, right: right}
+        return mk_bin_exp(ope, reduce(left), right) unless left.val?
+        return mk_bin_exp(ope, left, reduce(right)) unless right.val?
+
+        return mk_atom(left == right ? 'true' : 'false') if ope == '='
+
+        [left, right] => [{type:'nat', value:left_val}, {type:'nat', value:right_val}]
+        case ope
+        in '+'
+            return mk_nat(left_val + right_val)
+        in '-' if left_val < right_val
+            return mk_nat(0)
+        in '-'
+            return mk_nat(left_val - right_val)
+        in '*'
+            return mk_nat(left_val * right_val)
+        in '/'
+            return mk_nat(left_val / right_val)
         end
     end
 end
--- spec/actor_evaluator_spec.rb
+++ spec/actor_evaluator_spec.rb
@@ -16,4 +16,42 @@ RSpec.describe do
         ast = @parser.parse('+(1, +(1,1))').ast
         expect(reduce(ast)).to eq(mk_bin_exp('+', mk_nat(1), mk_nat(2)))
     end
+    it '-(2, 1) -> 1' do
+        ast = @parser.parse('-(2, 1)').ast
+        expect(reduce(ast)).to eq(mk_nat(1))
+    end
+    it '-(2, 3) -> 0 : 引かれる数より引く数の方が大きければ 0' do
+        ast = @parser.parse('-(2, 3)').ast
+        expect(reduce(ast)).to eq(mk_nat(0))
+    end
+    it '-(2, -(1, 1)) -> -(2, 0)' do
+        ast = @parser.parse('-(2, -(1,1))').ast
+        expect(reduce(ast)).to eq(mk_bin_exp('-', mk_nat(2), mk_nat(0)))
+    end
+    it '*(3, 4) -> 12' do
+        ast = @parser.parse('*(3, 4)').ast
+        expect(reduce(ast)).to eq(mk_nat(12))
+    end
+
+    # 0除算は考えない！ /(4, 0) とかすると多分エラー出るけど気にしない
+    it '/(4, 2) -> 2' do
+        ast = @parser.parse('/(4, 2)').ast
+        expect(reduce(ast)).to eq(mk_nat(2))
+    end
+    it '/(4, 3) -> 1' do
+        ast = @parser.parse('/(4, 3)').ast
+        expect(reduce(ast)).to eq(mk_nat(1))
+    end
+    it '=(4, 3) -> False' do
+        ast = @parser.parse('=(4, 3)').ast
+        expect(reduce(ast)).to eq(mk_atom('false'))
+    end
+    it '=(pair(1, 2), pair(1, 2)) -> True' do
+        ast = @parser.parse('=(pair(1,2), pair(1,2))').ast
+        expect(reduce(ast)).to eq(mk_atom('true'))
+    end
+    it '=(λx.x, 3) -> False' do
+        ast = @parser.parse('=(λx.x, 3)').ast
+        expect(reduce(ast)).to eq(mk_atom('false'))
+    end
 end
