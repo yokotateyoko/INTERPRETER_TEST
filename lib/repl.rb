48409dd00d35db0c743d795518a3139485b607bb
mohira <shukatsudaison@gmail.com>
2023-09-21 18:42:42 +0900

    add: REPLのカーソル移動およびHistory機能。これでREPLが楽しくなる！
    - Windowsに対応しているかは未検証です。macOSでは動作を確認しています。
    - Rubyのコードのスタイルはほぼいじっていません。たとえば、`puts ast.to_s`と`puts ast`や例外処理の記述などです。

--- lib/repl.rb
+++ lib/repl.rb
@@ -1,20 +1,18 @@
+require 'readline'
 require_relative 'actor_parser'
 require_relative 'actor_evaluator'
 
-def readline
-    print '> '
-    $stdin.gets.strip
-end
 parser = Parser.new
-while line = readline
-    break if line == 'exit'
-    begin
-        ast = parser.parse(line).ast
-        until ast.val?
-            ast = reduce(ast)
-        end
-        puts ast.to_s
-    rescue
-        puts "ERROR: " + $!.message
-    end
+
+while (line = Readline.readline('> ', true))
+  break if line == 'exit'
+
+  begin
+    ast = parser.parse(line).ast
+    ast = reduce(ast) until ast.val?
+
+    puts ast.to_s
+  rescue
+    puts "ERROR: " + $!.message
+  end
 end
